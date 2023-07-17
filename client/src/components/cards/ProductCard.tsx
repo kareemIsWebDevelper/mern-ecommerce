5a18bc66501d911850336bfa973714005f803e70
Kareem Khaled <122558256+kareemIsWebDevelper@users.noreply.github.com>
2023-07-17 13:24:17 +0300

    Update ProductCard.tsx

--- client/src/components/cards/ProductCard.tsx
+++ client/src/components/cards/ProductCard.tsx
@@ -1,32 +1,33 @@
 import { Link } from "react-router-dom";
 import AddToCartButton from "../cart/AddToCartButton";
 
-const ProductCard = ({ products }) => {
+export const ProductCard = ({ products, isVisible }) => {
   return (
-    <section className="gap-8">
-      {products.map((p, _id) => (
-        <div
-          key={_id}
-          className="bg-white gridStart mb-4 w-82 h-fit p-4 border shadow-lg cursor-pointer"
-        >
-          <img className="w-56 h-44" src={p.image} alt="product" />
-          <div className="flexAround gap-2">
-            <h4 className="font-extrabold">
-              ${p.discount}
-            </h4>
-            <span
-              className="font-light line-through text-slate-400 text-sm md:text-lg">
+      <section className="gap-8">
+        {isVisible && <p className="text-center">Loading Products...</p>}
+        {products.map((p, _id) => (
+            <div
+                key={_id}
+                className="bg-white gridStart mb-4 w-82 h-fit p-4 border shadow-lg cursor-pointer"
+            >
+              <Link to={`product/${p._id}`}>
+                  <img className="w-56 h-44" src={p.image} alt="product" />
+              </Link>
+              <div className="flexAround gap-2">
+                <h4 className="font-extrabold">
+                  ${p.discount}
+                </h4>
+                <span
+                    className="font-light line-through text-slate-400 text-sm md:text-lg">
               ${p.price}
             </span>
-          </div>
-          <h4 className="text-center">
-            {p.name}
-          </h4>
-          <AddToCartButton productId={p._id} />
-        </div>
-      ))}
-    </section>
+              </div>
+              <h4 className="text-center">
+                {p.name}
+              </h4>
+              <AddToCartButton productId={p._id} />
+            </div>
+        ))}
+      </section>
   );
 };
-
-export default ProductCard;
